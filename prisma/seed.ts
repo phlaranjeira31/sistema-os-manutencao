2851391dd04e26a4d95ea9c75cb7c405e11732f7
phlaranjeira <pedrolaranjeira09@gmail.com>
2026-05-28 09:24:27 -0300

    fix prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,8 @@
 import "dotenv/config";
 import { PrismaClient, PerfilUsuario } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcryptjs";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 async function main() {
   const senhaHash = await bcrypt.hash("admin123", 10);
