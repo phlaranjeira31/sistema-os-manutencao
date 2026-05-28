cf1bd06f7b7b9a2512cabcf024a4ceefd23166d4
phlaranjeira <pedrolaranjeira09@gmail.com>
2026-05-28 08:20:24 -0300

    fix types preventiva

--- app/admin/os/preventivas/lista/page.tsx
+++ app/admin/os/preventivas/lista/page.tsx
@@ -73,7 +73,7 @@ export default async function ListaPreventivasPage() {
           </section>
         ) : (
           <section className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-            {preventivas.map((preventiva) => (
+            {preventivas.map((preventiva: any) => (
               <CardPreventiva
                 key={preventiva.id}
                 preventiva={preventiva}
