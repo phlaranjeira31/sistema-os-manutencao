908183cee609d7b1ae0cca8cdbaba7b1ffb10443
phlaranjeira <pedrolaranjeira09@gmail.com>
2026-05-28 14:26:32 -0300

    fix next image import

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,6 +14,7 @@ import {
 
 import { Suspense, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
